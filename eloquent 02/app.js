1688ae329f30faee2c0c656959cf1743ed0aeba5
Eiyuulia <okkyprasetian@gmail.com>
2021-09-27 09:20:36 +0700

    finish chapter function in javascript eloquent

---
+++ eloquent 02/app.js
@@ -0,0 +1,23 @@
+// Recursive function for determine number is odd or even
+
+// function isEven(n){
+//     if (n==0) return true
+//     if (n==1) return false
+//     if (n<0) return isEven(n+2)
+//     return isEven(n-2)
+// }
+
+
+// Function bean counting
+
+// function countChar(word, c){
+//     let x = 0
+//     for(let i=0;i<word.length;i++){
+//         if (word[i] == c) x++
+//     }
+//     return x
+// }
+
+
+
+
