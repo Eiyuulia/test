9f17b9723ff3fd38c3e1fb893ba0e64abd0987fd
Eiyuulia <okkyprasetian@gmail.com>
2021-09-27 10:49:06 +0700

    aadd

--- eloquent 03/app.js
+++ eloquent 03/app.js
@@ -1,3 +1,13 @@
 function so(){
     return (Math.round(Math.random() * 10))
 }
+
+
+function range(start, end){
+    let arr = []
+
+    for(let i = start; i<=end;i++){
+        arr.push(i)
+    }
+    return arr
+}
