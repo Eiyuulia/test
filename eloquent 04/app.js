db54440eb1b89f80502d31631b7510b55301b74d
Eiyuulia <okkyprasetian@gmail.com>
2021-09-28 09:21:00 +0700

    finish javascript eloquent chapter high-order function

---
+++ eloquent 04/app.js
@@ -0,0 +1,25 @@
+// // Start - Function Flatter
+
+// let arr = ['aku','mau','kesana']
+// let arr2 = ['disini', 'sangat','dingin','sekali','tapi']
+
+// function reduce(arr){
+//     return arr.reduce((bfr,aft)=> bfr.concat(aft))
+// }
+
+// // End - Function Flatter
+
+// // Start - Function Everything
+
+// function every(arr, func){
+//     arr.forEach(e=>{
+//         if(func(e) != true) return false
+//     })
+//     return true
+// }
+
+// let eArr = [14,12,13,15]
+
+// console.log(every(eArr,e => e < 17))
+
+// // End - Function Everything
